c6358cf4019357f5eb5649e30c3cc835c91c1cbd
Jaewoong2 <jwisgenius@naver.com>
2026-08-05 00:01:58 +0900

    refactor(dp): 바닥 공사(타일 만들기) 점화식 단순화

--- dp_실전4_타일만들기/solution.py
+++ dp_실전4_타일만들기/solution.py
@@ -29,7 +29,7 @@ def solution():
     dp[2] = 3
 
     for i in range(3, n + 1):
-        dp[i] = (dp[i - 1] * dp[1]) + (dp[i - 2] * (dp[2] - 1))
+        dp[i] = (dp[i - 1] * 1) + (dp[i - 2] * (2))
 
     return dp[n]
 
