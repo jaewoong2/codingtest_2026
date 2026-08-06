461cb23266190698ce5a743e9e8e12dac45db422
Jaewoong2 <jwisgenius@naver.com>
2026-08-06 21:04:54 +0900

    feat(greedy): add 만들 수 없는 금액 (이코테)

---
+++ 이코테_만들수 없는 금액/solution.py
@@ -0,0 +1,36 @@
+import sys
+
+# ==============================================================================
+# 꿀팁: 파이썬 코드 자체에서 같은 폴더의 input.txt가 있으면 자동으로 읽게 만드는 코드
+# (백준/프로그래머스에 제출할 때도 이 코드를 그대로 제출해도 자동으로 표준입력을 받아 정상 동작합니다!)
+# ==============================================================================
+import os
+
+if os.path.exists(os.path.join(os.path.dirname(__file__), "input.txt")):
+    sys.stdin = open(os.path.join(os.path.dirname(__file__), "input.txt"), "r")
+
+input = sys.stdin.readline
+
+# 1 2 3
+
+
+def solution():
+    n = int(input())
+    array = [x for x in map(int, input().split())]
+    array.sort()
+
+    target = 1
+
+    # target - 1 까지는 만들 수 있음
+    for number in array:
+        if number > target:
+            break
+
+        target = target + number
+
+    return target
+
+
+if __name__ == "__main__":
+    result = solution()
+    print(result)
