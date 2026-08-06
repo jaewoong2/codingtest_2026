64480f8839dd63e7ed0a39ae8760162bd2615011
Jaewoong2 <jwisgenius@naver.com>
2026-08-06 21:04:54 +0900

    feat(greedy): add 곱하기 혹은 더하기 (이코테)

---
+++ 이코테_곱하기혹은더하기/solution.py
@@ -0,0 +1,30 @@
+import sys
+
+# ==============================================================================
+# 꿀팁: 파이썬 코드 자체에서 같은 폴더의 input.txt가 있으면 자동으로 읽게 만드는 코드
+# (백준/프로그래머스에 제출할 때도 이 코드를 그대로 제출해도 자동으로 표준입력을 받아 정상 동작합니다!)
+# ==============================================================================
+import os
+
+if os.path.exists(os.path.join(os.path.dirname(__file__), "input.txt")):
+    sys.stdin = open(os.path.join(os.path.dirname(__file__), "input.txt"), "r")
+
+input = sys.stdin.readline
+
+
+def solution():
+    string = [int(x) for x in str(int(str(input())))]
+    result = string[0]
+
+    for number in string[1:]:
+        if number == 0 or number == 1 or result == 1:
+            result += number
+        else:
+            result *= number
+
+    return result
+
+
+if __name__ == "__main__":
+    result = solution()
+    print(result)
