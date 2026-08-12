a7d6214cd11cd80ef505a1f6a41da35d3054db8d
Jaewoong2 <jwisgenius@naver.com>
2026-08-12 12:58:39 +0900

    feat(dfs-bfs): add 인구 이동 solution (이코테)

---
+++ 이코테_인구이동/solution.py
@@ -0,0 +1,73 @@
+import sys
+
+# ==============================================================================
+# 꿀팁: 파이썬 코드 자체에서 같은 폴더의 input.txt가 있으면 자동으로 읽게 만드는 코드
+# (백준/프로그래머스에 제출할 때도 이 코드를 그대로 제출해도 자동으로 표준입력을 받아 정상 동작합니다!)
+# ==============================================================================
+import os
+
+if os.path.exists(os.path.join(os.path.dirname(__file__), "input.txt")):
+    sys.stdin = open(os.path.join(os.path.dirname(__file__), "input.txt"), "r")
+
+input = sys.stdin.readline
+
+
+def solution():
+    n, l, r = map(int, input().split())
+    maps = [[x for x in map(int, input().split())] for _ in range(n)]
+
+    count = 0
+    moves = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+    def dfs(row, col, visited):
+        for dr, dc in moves:
+            nr, nc = row + dr, col + dc
+
+            if 0 <= nr < n and 0 <= nc < n:
+                if (nr, nc) not in visited:
+                    if l <= maps[row][col] - maps[nr][nc] <= r:
+                        visited.add((nr, nc))
+                        dfs(nr, nc, visited)
+
+                    elif l <= maps[nr][nc] - maps[row][col] <= r:
+                        visited.add((nr, nc))
+                        dfs(nr, nc, visited)
+
+    count = 0
+    while True:
+        all_visited = set()
+        nodes = []
+
+        for i in range(n):
+            for j in range(n):
+                if (i, j) not in all_visited:
+                    visited = set()
+                    visited.add((i, j))
+                    dfs(i, j, visited)
+
+                    for a, b in visited:
+                        all_visited.add((a, b))
+
+                    nodes.append(visited)
+
+        is_break = True
+        for values in nodes:
+            if len(values) > 1:
+                is_break = False
+                total = sum(maps[a][b] for a, b in values)
+                result = total // len(values)
+
+                for a, b in values:
+                    maps[a][b] = result
+
+        if is_break:
+            break
+
+        count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    result = solution()
+    print(result)
